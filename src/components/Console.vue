bbfedb04e2e3e4252fcf7dc182056a54cbb39019
ChengYuXuan <MF20320022@smail.nju.edu.cn>
2020-12-16 15:48:25 +0800

    init

---
+++ src/components/Console.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Console
+        </h1>
+        <v-text-field v-model="command" outlined clearable label="$>">
+        </v-text-field>
+        <v-btn color="primary" @click="sendCommand">Confirm</v-btn>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <v-textarea clearable readonly :value="output" outlined>
+
+        </v-textarea>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data(){
+      return{
+        command: '',
+        output: 'output'
+      }
+    },
+    methods:{
+      sendCommand(){
+        console.log(this.command)
+      }
+    },
+    beforeCreate() {
+
+    }
+  }
+</script>
